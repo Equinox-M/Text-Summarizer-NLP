8e12587d4bc3ca3e5e80cea673b390810288922d
MehdiX <mehdix@MehdiXs-MacBook-Pro.local>
2024-05-02 17:04:55 +0800

    Data Validation pipeline completed

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 1. Update config.yaml
 2. Update params.yaml
 3. Update entity
-4. Update the Configuration managaer in the src config
+4. Update the Configuration manager in the src config
 5. Update the components
 6. Update the pipeline in the src pipeline
 7. Update the main.py
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from src.textSummarizer.pipeline.stage_1_data_ingestion import DataIngestionTrainingPipeline
 from src.textSummarizer.logging import logger
+from src.textSummarizer.pipeline.stage_2_data_validation import DataValidationTrainingPipeline
+
+
 
 STAGE_NAME = 'Data Ingestion Stage'
 try:
@@ -11,3 +14,14 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_validation = DataValidationTrainingPipeline()
+   data_validation.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
+except Exception as e:
+        logger.exception(e)
+        raise e
